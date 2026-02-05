0217ecb6d7ef2a49b2708c74bc83a22a53153fc4
Kelvin <144894229+KelvinAlviero@users.noreply.github.com>
2026-02-05 11:46:03 +0700

    Movement_1
    Deleted unecesarry things

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D body;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
