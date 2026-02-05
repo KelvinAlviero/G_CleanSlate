61c2c7cf412b0b09f822e76c6fea58e37d824001
Kelvin <144894229+KelvinAlviero@users.noreply.github.com>
2026-02-05 13:31:59 +0700

    Movement update MK1
    Still needs fixing

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,16 +2,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D body;
+    public Rigidbody2D rb;
+    public float speed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        body = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        //Tracks the player's movement keys
+        rb = GetComponent<Rigidbody2D>();
+        float moveX = Input.GetAxis("Horizontal");    
+        float moveY = Input.GetAxis("Vertical"); 
+
+        //Saves the player's movement keys into variables
+        Vector2 moveDirection = new Vector2 (moveX, moveY);
+
+        //Used to smooth the speed, needs fixing
+        if(moveDirection.magnitude > 0)
+        {
+            rb.linearVelocity = moveDirection.normalized*speed;
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    
     }
+    
 }
